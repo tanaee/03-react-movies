65860b0600168365ffeaf5cc115f00ac871544f7
tanaee <tanaaeeee@gmail.com>
2025-08-12 20:17:57 +0300

    done

---
+++ src/components/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
+import MovieGrid from '../components/MovieGrid/MovieGrid';
+import Loader from '../components/Loader/Loader';
+import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
+import MovieModal from '../components/MovieModal/MovieModal';
+import { fetchMovies } from './services/movieService';
+import type { Movie } from './types/movie';
+import toast from 'react-hot-toast';
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    setMovies([]);
+    setError(false);
+    setLoading(true);
+    try {
+      const data = await fetchMovies(query);
+      if (data.length === 0) {
+        toast.error('No movies found for your request.');
+      }
+      setMovies(data);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {!loading && !error && (
+        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
+  );
+}
---
+++ src/components/services/movieService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import type { Movie } from '../types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  const config = {
+    params: { query },
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  };
+
+  try {
+    const response: AxiosResponse<FetchMoviesResponse> = await axios.get(
+      `${BASE_URL}/search/movie`,
+      config
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
